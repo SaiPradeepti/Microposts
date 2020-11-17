361c52aab9512aa48b5755e87342ecddb5d52bbb
SaiPradeepti <saipradeepti14@gmail.com>
2020-11-17 20:08:35 +0530

    Adding Get and Display posts features

---
+++ src/app.js
@@ -0,0 +1,14 @@
+//run...npm start
+//npm run json:server
+
+import { http } from './http';
+import { ui } from './ui';
+
+// Get posts on DOM load
+document.addEventListener('DOMContentLoaded', getPosts);
+
+function getPosts(){
+    http.get('http://localhost:3000/posts')
+        .then(data => ui.showPosts(data))
+        .catch(err => console.log(err));
+}

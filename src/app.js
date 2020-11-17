19aa39dcb34a4b72dd9b4a3df0f5b65ab459c209
SaiPradeepti <saipradeepti14@gmail.com>
2020-11-17 23:17:38 +0530

    Add posts and show alert

--- src/app.js
+++ src/app.js
@@ -7,8 +7,27 @@ import { ui } from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', addPost);
+
+// Get Posts
 function getPosts(){
     http.get('http://localhost:3000/posts')
         .then(data => ui.showPosts(data))
         .catch(err => console.log(err));
+}
+
+// Add Posts
+function addPost(){
+    http.post('http://localhost:3000/posts',{
+        "id": document.getElementById('id').value,
+        "title": document.getElementById('title').value,
+        "body": document.getElementById('body').value
+    })
+    .then(data => {
+        ui.showAlert('Post added', 'alert alert-success');
+        ui.clearFields();
+        getPosts();
+    })
+    .catch(err => console.log(err));
 }
